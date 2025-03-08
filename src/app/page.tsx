744cf66483f4c85523be38832228f6c963174fba
Evan Lin <elin@middlebury.edu>
2025-03-08 15:51:53 -0500

    image in rectangle fit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import ImageInRectangle from "../../components/imageInRectangle";
 
 export default function Home() {
+  const imageUrl = "https://www.mdxblog.io/images/posts/how-to-use-images/grass-tree-sky.jpg";
   return (
-    <div>
-      StepLy
+    <div className="container">
+      <div className="rectangle">
+        <ImageInRectangle imageUrl = {imageUrl}/>
+      </div>
     </div>
   );
 }
