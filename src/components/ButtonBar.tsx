dcb92137cdd92d2c1ddcfb415637f125cb735b0e
Prashanth Babu <Prashanthbabu1329@gmail.com>
2025-03-08 11:30:09 -0500

    moved button bar to bottom of page.

--- src/components/ButtonBar.tsx
+++ src/components/ButtonBar.tsx
@@ -2,7 +2,7 @@ import { ArrowLeftToLine, Play, ArrowRightToLine }  from 'lucide-react';
 
 export default function ButtonBar() {
     return (
-        <div className='buttonBarContainer'>
+        <div className='fixed bottom-0 left-1/2 -translate-x-1/2 flex justify-around w-1/2 border border-[#C6C6C6] rounded-2xl py-2'>
             <button className='buttonBar'><ArrowLeftToLine/></button>
             <button className='buttonBar'><Play/></button>
             <button className='buttonBar'><ArrowRightToLine/></button>
