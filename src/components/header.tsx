e0df4856cb38d858b855281fa0c24f72687f5f3b
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-04-04 16:36:59 -0400

    using new image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="flex justify-center items-center w-screen h-screen">
       <div className="flex justify-center items-center w-[90%] h-[85%]">
         <img
-          src="/sampleImages/oracle.png"
+          src="/sampleImages/oracle1.png"
           alt="platform screenshot for tutorial"
           className="w-full h-full object-contain" // using object-contain to make sure the image fits the container and doesn't stretch
         />
