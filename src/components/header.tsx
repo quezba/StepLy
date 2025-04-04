184f6ba134c7905ee4225fceefc624416a4d2740
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-04-04 16:21:56 -0400

    restructure per structure.md

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,12 +6,12 @@ export default function Header() {
     <div className="flex justify-center items-center w-screen h-screen">
       <div className="flex justify-center items-center w-[90%] h-[85%]">
         <img
-          src="/images/oracle.png"
+          src="/sampleImages/oracle.png"
           alt="platform screenshot for tutorial"
           className="w-full h-full object-contain" // using object-contain to make sure the image fits the container and doesn't stretch
         />
       </div>
-      
+
     </div>
   );
 }
