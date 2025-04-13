f8dc9d9759a7a7744777c22686b24dd06b257f14
DanteAguilarB <142114920+DanteAguilarB@users.noreply.github.com>
2025-04-13 15:28:28 -0400

    fetching from local files

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,26 @@
 "use client";
-
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [img, setImg] = useState();
+
+  const fetchImage = async () => {
+    const res = await fetch("/sampleImages/oracle0.png");
+    const imageBlob = await res.blob();
+    console.log(imageBlob);
+    const imageObjectURL = URL.createObjectURL(imageBlob);
+    setImg(imageObjectURL);
+  };
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+  
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <div className="flex justify-center items-center w-[90%] h-[85%]">
         <img
-          src="/sampleImages/oracle0.png"
+          src={img}
           alt="platform screenshot for tutorial"
           className="w-full h-full object-contain" // using object-contain to make sure the image fits the container and doesn't stretch
         />
